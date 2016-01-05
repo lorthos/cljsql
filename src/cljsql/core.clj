f130d1719fb7ad9614a1fc1c497e52aa2869762e
Halit Olali <halitolali@gmail.com>
2016-01-05 21:44:39 +0100

    instaparse test

--- src/cljsql/core.clj
+++ src/cljsql/core.clj
@@ -1,6 +1,20 @@
-(ns cljsql.core)
+(ns cljsql.core
+  (:require [instaparse.core :as insta]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def parse-sql
+  (insta/parser
+    "SQL                = SELECT_QUERY
+     <SELECT_QUERY>     = <SELECT> FIELDS <FROM> <TABLE> WHERE_CLAUSE?  ORDER_CLAUSE?
+     SELECT             = 'select '
+     FIELDS             = (FIELD)+ | '* '
+     <FIELD>            = #'[a-z|_|(|)]+'  <#' '+>
+     FROM               = 'from '
+     TABLE              = #'[a-z|_|0-9]+' <' '>
+     PREDICATE          = (#'[a-z|A-Z|_| |(|)|>|<|=|!|0-9|?|\\'|%]+')
+     WHERE              = 'where '
+     WHERE_AND          = ' and '
+     WHERE_CLAUSE       = <WHERE> PREDICATE {WHERE_AND PREDICATE}
+     ORDER_STMT         = 'order by '
+     ORDER_CLAUSE       = <ORDER_STMT>  (#'[a-z|A-Z|_| |>|<|=|0-9|?|\\'|%]+')
+     "
+    :string-ci true))
--- test/cljsql/core_test.clj
+++ test/cljsql/core_test.clj
@@ -2,6 +2,26 @@
   (:require [clojure.test :refer :all]
             [cljsql.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn sql [stmt] (parse-sql stmt))
+(defn sql! [stmt] (parse-sql stmt :unhide :content))
+
+(deftest parsing-test
+  (testing "select *"
+    (is (= (sql "select * from table ")
+           [:SQL
+            [:FIELDS
+             "* "]]
+           ))
+    (is (= (sql "select * from table where x=y")
+           [:SQL
+            [:FIELDS
+             "* "]
+            [:WHERE_CLAUSE
+             [:PREDICATE
+              "x=y"]]]
+           ))
+    ;todo parse predicates properly
+    (is (= (sql "select * from table where x=y and 1=1")
+           nil
+           ))
+    ))
